6b4423296f9ac222d034f8f3b8cc9811d8f2d2e8
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-08 20:50:37 -0400

    added nav breadcrumbs for feature panel

---
+++ src/components/FeaturePanel/Breadcrumbs.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Flex } from '@chakra-ui/react';
+import Crumb from './Crumb';
+
+function Breadcrumbs({ data, current }) {
+
+    return (
+        <Flex width={200} height={45} align='center' justify={'space-evenly'} mt={6}>
+            {data.map((item, index) => 
+                <Crumb id={item.id} index={index} current={current}/>
+            )}
+        </Flex>
+    )
+}
+
+export default Breadcrumbs
