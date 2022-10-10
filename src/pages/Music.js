5b13cc4bb7133391f93bf4b87c64ba790f110a9a
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-10 13:58:42 -0400

    added the audio card component to the music page

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,18 +1,22 @@
 import { ArrowRightIcon } from '@chakra-ui/icons'
-import { Flex, Heading, IconButton } from '@chakra-ui/react'
+import { Flex, Heading, IconButton, SimpleGrid } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import AudioCard from '../components/AudioCard/AudioCard';
 import SideBar from './../components/drawer/SideBar';
+import { useAudioFile } from './../context/AudioFileContext';
 
 function Music() {
     const [isToggled, setIsToggled] = useState(false)
+    const { files } = useAudioFile()
 
     const handleChange = (value) => {
       setIsToggled(value)
     }
   
     return (
+      
       <Flex overflowX={'hidden'} width={'100vw'} height={'100vh'} direction={'row'} bg={'main.black'} >
-  
+        
         <Flex width={45} height={'100%'} bg={'main.white'} align={'center'} justify={'center'}>
           <IconButton
             height={200}
@@ -33,9 +37,11 @@ function Music() {
         </Flex>
         
         <Flex justify='center' align='center' height={'100%'} width={'100%'}>
-          <Flex>
-            <Heading color={'main.white'}> Music Page </Heading>
-          </Flex>
+          <SimpleGrid columns={2} spacing={4}>
+            {files.map((song) => 
+              <AudioCard data={song}/>
+            )}
+            </SimpleGrid>
         </Flex>
   
       </Flex>
